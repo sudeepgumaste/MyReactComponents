8da6bee70917bd20a561d23547c4da4638c89442
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-09-14 22:46:16 +0530

    Added layout loader component

---
+++ src/components/LayoutLoader/LayoutLoader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './layout-loader.scss';
+
+const LayoutLoader = (props) => {
+  return <div className='layout-loader' {...props}></div>;
+};
+
+export default LayoutLoader;
---
+++ src/stories/LayoutLoader.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutLoader from '../components/LayoutLoader/LayoutLoader';
+
+export default { title: 'LayoutLoader' };
+
+export const layoutLoader = () => (
+  <LayoutLoader style={{
+    borderRadius: '0.25rem',
+    height: '2.5rem',
+    width: '25rem'
+  }}/>
+)

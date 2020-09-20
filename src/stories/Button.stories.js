69c4a842bdc883bb92110d4a7e59b435110357ab
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-09-20 10:38:12 +0530

    Added outline button

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../components/Button/Button';
 import TextButton from '../components/Button/TextButton';
+import OutlineButton from '../components/Button/OutlineButton';
 
 export default { title: 'Button' };
 
@@ -33,5 +34,21 @@ export const textButton = () => (
     <TextButton onClick={()=>{console.log('Click')}} disabled>
       Primary Button Disabled
     </TextButton>
+  </>
+);
+
+export const outlineButton = () => (
+  <>
+    <OutlineButton
+      onClick={()=>{console.log('Click')}}
+      style={{
+        marginRight: '1rem'
+      }}
+    >
+      Primary Text Button
+    </OutlineButton>
+    <OutlineButton onClick={()=>{console.log('Click')}} disabled>
+      Primary Button Disabled
+    </OutlineButton>
   </>
 );
